d1a304cb9f4c88598731d27749d80c3e88914c44
Fran Tena <tarod.net@gmail.com>
2016-11-14 00:01:30 +0100

    change database field names

--- initdb.h
+++ initdb.h
@@ -53,25 +53,25 @@
 
 #include <QtSql>
 
-void addBook(QSqlQuery &q, const QString &title, int year, const QVariant &authorId,
-             const QVariant &genreId, int rating)
+void addOrder(QSqlQuery &q, const QString &name, int year, const QVariant &supplierId,
+             const QVariant &productId, int rating)
 {
-    q.addBindValue(title);
+    q.addBindValue(name);
     q.addBindValue(year);
-    q.addBindValue(authorId);
-    q.addBindValue(genreId);
+    q.addBindValue(supplierId);
+    q.addBindValue(productId);
     q.addBindValue(rating);
     q.exec();
 }
 
-QVariant addGenre(QSqlQuery &q, const QString &name)
+QVariant addProduct(QSqlQuery &q, const QString &name)
 {
     q.addBindValue(name);
     q.exec();
     return q.lastInsertId();
 }
 
-QVariant addAuthor(QSqlQuery &q, const QString &name, const QDate &birthdate)
+QVariant addSupplier(QSqlQuery &q, const QString &name, const QDate &birthdate)
 {
     q.addBindValue(name);
     q.addBindValue(birthdate);
@@ -90,49 +90,55 @@ QSqlError initDb()
     if (!db.open())
         return db.lastError();
 
+    /* TESTING */
+    // TODO: remove after testing
+    QSqlQuery qTesting;
+    if (!qTesting.exec(QLatin1String("DROP TABLE IF EXISTS orders, suppliers, products")))
+        return qTesting.lastError();
+
     QStringList tables = db.tables();
-    if (tables.contains("books", Qt::CaseInsensitive)
-        && tables.contains("authors", Qt::CaseInsensitive))
+    if (tables.contains("orders", Qt::CaseInsensitive)
+        && tables.contains("suppliers", Qt::CaseInsensitive))
         return QSqlError();
 
     QSqlQuery q;
-    if (!q.exec(QLatin1String("create table books(id serial primary key, title varchar, author integer, genre integer, year integer, rating integer)")))
+    if (!q.exec(QLatin1String("create table orders(id serial primary key, name varchar, supplier integer, product integer, year integer, rating integer)")))
         return q.lastError();
-    if (!q.exec(QLatin1String("create table authors(id serial primary key, name varchar, birthdate date)")))
+    if (!q.exec(QLatin1String("create table suppliers(id serial primary key, name varchar, birthdate date)")))
         return q.lastError();
-    if (!q.exec(QLatin1String("create table genres(id serial primary key, name varchar)")))
+    if (!q.exec(QLatin1String("create table products(id serial primary key, name varchar)")))
         return q.lastError();
 
-    if (!q.prepare(QLatin1String("insert into authors(name, birthdate) values(?, ?)")))
+    if (!q.prepare(QLatin1String("insert into suppliers(name, birthdate) values(?, ?)")))
         return q.lastError();
-    QVariant asimovId = addAuthor(q, QLatin1String("Isaac Asimov"), QDate(1920, 2, 1));
-    QVariant greeneId = addAuthor(q, QLatin1String("Graham Greene"), QDate(1904, 10, 2));
-    QVariant pratchettId = addAuthor(q, QLatin1String("Terry Pratchett"), QDate(1948, 4, 28));
+    QVariant asimovId = addSupplier(q, QLatin1String("Isaac Asimov"), QDate(1920, 2, 1));
+    QVariant greeneId = addSupplier(q, QLatin1String("Graham Greene"), QDate(1904, 10, 2));
+    QVariant pratchettId = addSupplier(q, QLatin1String("Terry Pratchett"), QDate(1948, 4, 28));
 
-    if (!q.prepare(QLatin1String("insert into genres(name) values(?)")))
+    if (!q.prepare(QLatin1String("insert into products(name) values(?)")))
         return q.lastError();
-    QVariant sfiction = addGenre(q, QLatin1String("Science Fiction"));
-    QVariant fiction = addGenre(q, QLatin1String("Fiction"));
-    QVariant fantasy = addGenre(q, QLatin1String("Fantasy"));
+    QVariant sfiction = addProduct(q, QLatin1String("Science Fiction"));
+    QVariant fiction = addProduct(q, QLatin1String("Fiction"));
+    QVariant fantasy = addProduct(q, QLatin1String("Fantasy"));
 
-    if (!q.prepare(QLatin1String("insert into books(title, year, author, genre, rating) values(?, ?, ?, ?, ?)")))
+    if (!q.prepare(QLatin1String("insert into orders(name, year, supplier, product, rating) values(?, ?, ?, ?, ?)")))
         return q.lastError();
-    addBook(q, QLatin1String("Foundation"), 1951, asimovId, sfiction, 3);
-    addBook(q, QLatin1String("Foundation and Empire"), 1952, asimovId, sfiction, 4);
-    addBook(q, QLatin1String("Second Foundation"), 1953, asimovId, sfiction, 3);
-    addBook(q, QLatin1String("Foundation's Edge"), 1982, asimovId, sfiction, 3);
-    addBook(q, QLatin1String("Foundation and Earth"), 1986, asimovId, sfiction, 4);
-    addBook(q, QLatin1String("Prelude to Foundation"), 1988, asimovId, sfiction, 3);
-    addBook(q, QLatin1String("Forward the Foundation"), 1993, asimovId, sfiction, 3);
-    addBook(q, QLatin1String("The Power and the Glory"), 1940, greeneId, fiction, 4);
-    addBook(q, QLatin1String("The Third Man"), 1950, greeneId, fiction, 5);
-    addBook(q, QLatin1String("Our Man in Havana"), 1958, greeneId, fiction, 4);
-    addBook(q, QLatin1String("Guards! Guards!"), 1989, pratchettId, fantasy, 3);
-    addBook(q, QLatin1String("Night Watch"), 2002, pratchettId, fantasy, 3);
-    addBook(q, QLatin1String("Going Postal"), 2004, pratchettId, fantasy, 3);
+    addOrder(q, QLatin1String("Foundation"), 1951, asimovId, sfiction, 3);
+    addOrder(q, QLatin1String("Foundation and Empire"), 1952, asimovId, sfiction, 4);
+    addOrder(q, QLatin1String("Second Foundation"), 1953, asimovId, sfiction, 3);
+    addOrder(q, QLatin1String("Foundation's Edge"), 1982, asimovId, sfiction, 3);
+    addOrder(q, QLatin1String("Foundation and Earth"), 1986, asimovId, sfiction, 4);
+    addOrder(q, QLatin1String("Prelude to Foundation"), 1988, asimovId, sfiction, 3);
+    addOrder(q, QLatin1String("Forward the Foundation"), 1993, asimovId, sfiction, 3);
+    addOrder(q, QLatin1String("The Power and the Glory"), 1940, greeneId, fiction, 4);
+    addOrder(q, QLatin1String("The Third Man"), 1950, greeneId, fiction, 5);
+    addOrder(q, QLatin1String("Our Man in Havana"), 1958, greeneId, fiction, 4);
+    addOrder(q, QLatin1String("Guards! Guards!"), 1989, pratchettId, fantasy, 3);
+    addOrder(q, QLatin1String("Night Watch"), 2002, pratchettId, fantasy, 3);
+    addOrder(q, QLatin1String("Going Postal"), 2004, pratchettId, fantasy, 3);
 
     // Notifications
-    if (!q.exec(QLatin1String("DROP RULE IF EXISTS notifications ON books; CREATE RULE notifications AS ON UPDATE TO books DO NOTIFY dbupdated")))
+    if (!q.exec(QLatin1String("DROP RULE IF EXISTS notifications ON orders; CREATE RULE notifications AS ON UPDATE TO orders DO NOTIFY dbupdated")))
         return q.lastError();
 
     return QSqlError();
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,53 +1,3 @@
-/****************************************************************************
-**
-** Copyright (C) 2016 The Qt Company Ltd.
-** Contact: https://www.qt.io/licensing/
-**
-** This file is part of the demonstration applications of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:BSD$
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see https://www.qt.io/terms-conditions. For further
-** information use the contact form at https://www.qt.io/contact-us.
-**
-** BSD License Usage
-** Alternatively, you may use this file under the terms of the BSD license
-** as follows:
-**
-** "Redistribution and use in source and binary forms, with or without
-** modification, are permitted provided that the following conditions are
-** met:
-**   * Redistributions of source code must retain the above copyright
-**     notice, this list of conditions and the following disclaimer.
-**   * Redistributions in binary form must reproduce the above copyright
-**     notice, this list of conditions and the following disclaimer in
-**     the documentation and/or other materials provided with the
-**     distribution.
-**   * Neither the name of The Qt Company Ltd nor the names of its
-**     contributors may be used to endorse or promote products derived
-**     from this software without specific prior written permission.
-**
-**
-** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
-
 #include "mainwindow.h"
 #include "bookdelegate.h"
 #include "initdb.h"
@@ -78,24 +28,24 @@ MainWindow::MainWindow()
     model = new QSqlRelationalTableModel(ui.bookTable);
     //TODO: confirm! model->setEditStrategy(QSqlTableModel::OnManualSubmit);
     model->setEditStrategy(QSqlTableModel::OnFieldChange);
-    model->setTable("books");
+    model->setTable("orders");
 
     // Remember the indexes of the columns
-    authorIdx = model->fieldIndex("author");
-    genreIdx = model->fieldIndex("genre");
+    supplierIdx = model->fieldIndex("supplier");
+    productIdx = model->fieldIndex("product");
 
     // Set the relations to the other database tables
     /*
      * The QSqlRelation class stores information about an
      * SQL foreign key.
      */
-    model->setRelation(authorIdx, QSqlRelation("authors", "id", "name"));
-    model->setRelation(genreIdx, QSqlRelation("genres", "id", "name"));
+    model->setRelation(supplierIdx, QSqlRelation("suppliers", "id", "name"));
+    model->setRelation(productIdx, QSqlRelation("products", "id", "name"));
 
     // Set the localized header captions
-    model->setHeaderData(authorIdx, Qt::Horizontal, tr("Author Name"));
-    model->setHeaderData(genreIdx, Qt::Horizontal, tr("Genre"));
-    model->setHeaderData(model->fieldIndex("title"), Qt::Horizontal, tr("Title"));
+    model->setHeaderData(supplierIdx, Qt::Horizontal, tr("Supplier"));
+    model->setHeaderData(productIdx, Qt::Horizontal, tr("Product"));
+    model->setHeaderData(model->fieldIndex("name"), Qt::Horizontal, tr("Name"));
     model->setHeaderData(model->fieldIndex("year"), Qt::Horizontal, tr("Year"));
     model->setHeaderData(model->fieldIndex("rating"), Qt::Horizontal, tr("Rating"));
 
@@ -112,19 +62,19 @@ MainWindow::MainWindow()
     ui.bookTable->setSelectionMode(QAbstractItemView::SingleSelection);
 
     // Initialize the Author combo box
-    ui.authorEdit->setModel(model->relationModel(authorIdx));
-    ui.authorEdit->setModelColumn(model->relationModel(authorIdx)->fieldIndex("name"));
+    ui.supplierEdit->setModel(model->relationModel(supplierIdx));
+    ui.supplierEdit->setModelColumn(model->relationModel(supplierIdx)->fieldIndex("name"));
 
-    ui.genreEdit->setModel(model->relationModel(genreIdx));
-    ui.genreEdit->setModelColumn(model->relationModel(genreIdx)->fieldIndex("name"));
+    ui.productEdit->setModel(model->relationModel(productIdx));
+    ui.productEdit->setModelColumn(model->relationModel(productIdx)->fieldIndex("name"));
 
     QDataWidgetMapper *mapper = new QDataWidgetMapper(this);
     mapper->setModel(model);
     mapper->setItemDelegate(new BookDelegate(this));
-    mapper->addMapping(ui.titleEdit, model->fieldIndex("title"));
+    mapper->addMapping(ui.titleEdit, model->fieldIndex("name"));
     mapper->addMapping(ui.yearEdit, model->fieldIndex("year"));
-    mapper->addMapping(ui.authorEdit, authorIdx);
-    mapper->addMapping(ui.genreEdit, genreIdx);
+    mapper->addMapping(ui.supplierEdit, supplierIdx);
+    mapper->addMapping(ui.productEdit, productIdx);
     mapper->addMapping(ui.ratingEdit, model->fieldIndex("rating"));
 
     connect(ui.bookTable->selectionModel(), SIGNAL(currentRowChanged(QModelIndex,QModelIndex)),
@@ -159,12 +109,13 @@ void MainWindow::addAlbum()
 }
 
 void MainWindow::notificationHandler(const QString &name)
-{
-    QMessageBox::information(this, tr("notificationHandler"),
-            name);
+{    
+    qDebug() << Q_FUNC_INFO;
 
     // Populates the model with data from the table that was set via setTable().
-    emit model->select();
+    // TODO: valid way? emit model->select();
+
+    emit model->dataChanged(QModelIndex(), QModelIndex());
 }
 
 void MainWindow::createMenuBar()
